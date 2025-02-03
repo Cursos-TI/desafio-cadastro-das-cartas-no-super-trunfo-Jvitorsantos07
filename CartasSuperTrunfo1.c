afc8f7623cb5ab85c3250bb40c9e1fa28c5ca901
Jvitorsantos07 <joaovitorsantosdearaujo07@gmail.com>
2025-02-03 22:47:24 +0000

    desafio 1 super trunfo Cartas

--- CartasSuperTrunfo1.c
+++ CartasSuperTrunfo1.c
@@ -4,26 +4,30 @@ int main(){
 
     printf("Cartas Super Trunfo \n\n");
 
-    float populacao;
-    float area;
-    float pib;
+    float populacao, area, pib;
     int pontos_turisticos;
     char local[50];
 
     printf("Digite o local:\n");
-    scanf("%s", &local);
+    fgets(local, sizeof(local), stdin);
     
     printf("Digite o numero da populacao:\n");
-    scanf("%f", &populacao);
+    scanf("%g", &populacao);
 
     printf("Digite a area:\n");
-    scanf("%f", &area);
+    scanf("%g", &area);
 
     printf("Digite o PIB:\n");
-    scanf("%f", &pib);
+    scanf("%g", &pib);
 
     printf("Digite o numero de pontos turisticos: \n");
     scanf("%d", &pontos_turisticos);
+
+    printf("Local: %s\n", local);
+    printf("Populacao: %g\n", populacao);
+    printf("Area: %g\n", area);
+    printf("Pib: %g\n", pib);
+    printf("Pontos Turisticos: %d\n", pontos_turisticos);
 
     return 0;
     
