e4fb31e5a2341cd7c1107221e42a087f1fbb3634
Jvitorsantos07 <joaovitorsantosdearaujo07@gmail.com>
2025-03-13 23:10:58 +0000

    primeiro_desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,13 +2,15 @@
 
 int main(){
 
+    unsigned long int populacao1, populacao2;
     float pib_per_capita1, pib_per_capita2, densidade_populacional1, densidade_populacional2;
     char local1[70], local2[70];
     char codigo1, codigo2;
     char sigla1[10], sigla2[10];
-    int pontos_turisticos1, pontos_turisticos2, populacao1, populacao2;
+    int pontos_turisticos1, pontos_turisticos2;
     float area1, area2;
     float pib1, pib2;
+    float super_poder1, super_poder2;
 
 // usuário digita os dados da primeira carta//
 
@@ -23,7 +25,7 @@ printf("Digite a cidade do jogador 1:\n");
 fgets(local1, sizeof(local1), stdin);
 
 printf("Digite a populacao:\n");
-scanf("%d", &populacao1);
+scanf("%lu", &populacao1);
 
 printf("Digite o pib: \n");
 scanf("%f", &pib1);
@@ -36,6 +38,7 @@ scanf("%d", &pontos_turisticos1);
 
 densidade_populacional1 = pib1 / pontos_turisticos1;
 pib_per_capita1 = populacao1 / area1;
+super_poder1 = pontos_turisticos1 + pib1 + area1 + pontos_turisticos1 + pib_per_capita1 + densidade_populacional1;
 
 printf("\n");
 
@@ -54,7 +57,7 @@ printf("Digite o Nome da Cidade: \n");
 fgets(local2, sizeof(local2), stdin);
 
 printf("Digite a Populacao:\n");
-scanf("%d", &populacao2);
+scanf("%lu", &populacao2);
 
 printf(" Digite o PIB: \n");
 scanf("%f", &pib2);
@@ -65,8 +68,10 @@ scanf("%f", &area2);
 printf("Digite o numero de pontos turisticos:\n");
 scanf("%d", &pontos_turisticos2);
 
+//Calculos de densidade populacional2, pib per capita2 e super poder2//
 densidade_populacional2 = pib2 / pontos_turisticos2;
 pib_per_capita2 = populacao2 / area2;
+super_poder2 = pontos_turisticos2 + pib2 + area2 + pontos_turisticos2 + pib_per_capita2 + densidade_populacional2;
 
 
 
@@ -74,12 +79,12 @@ pib_per_capita2 = populacao2 / area2;
 
 printf("Cidades escolhidas foram: %s e %s. \n", local1 , local2);
 printf("Todos os dados a seguir serão RESPECTIVOS as cidades citadas acima.\n\n");
-printf("População carta1: %ld ,carta2: %ld\n", populacao1, populacao2);
+printf("População carta1: %lu ,carta2: %lu\n", populacao1, populacao2);
 printf("Os PIBs são de carta1 R$:%.2fM Reais, e carta2 R$:%.2fM Reais.\n", pib1, pib2);
 printf("Área carta1: %.3fkm², área carta2: %.3fkm².\n", area1, area2);
 printf("Pontos turisticos carta1: %d, carta2: %d.\n", pontos_turisticos1, pontos_turisticos2);
-printf("Densidade populacional é de: carta1 %.3f, e carta2: %.3lf.\n", densidade_populacional1, densidade_populacional2);
-printf("O Pib-Per-Capita da carta1 é:R$ %.3f, e carta2 é:R$ %.3f.\n");
+printf("Densidade populacional é de: carta1 %.2f, e carta2: %.3lf.\n", densidade_populacional1, densidade_populacional2);
+printf("O Pib-Per-Capita da carta1 é:R$ %.2f, e carta2 é:R$ %.3f.\n");
 
 if(densidade_populacional1 > densidade_populacional2){
     printf("Carta1 - %s Densidade populacional é %.1lf\n",local1, densidade_populacional1);
